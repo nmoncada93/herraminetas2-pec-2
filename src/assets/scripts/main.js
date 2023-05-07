8d89eae39f8a05cf059bce0e387b68e22f1aa696
[Nicolas Moncada Herrera] <[nmoncadaherrera@uoc.edu]>
2023-05-07 10:56:03 +0200

    Mi Web terminada

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,22 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
+
+//"Import npm dependencies": @import "bootstrap/scss/bootstrap.scss";
+
+
+
+/**
+ * Write any other JavaScript below
+ */
+
+
